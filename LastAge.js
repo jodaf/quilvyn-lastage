2858fc8ca2b065e8dcdeba57f48934854db16e2b
jhayes <jhayes>
2011-03-24 23:04:21 +0000

    Update copyrights.  Add a few optional training semicolons.  Add fbag and fset editor types to allow any field to be filtered.

--- LastAge.js
+++ LastAge.js
@@ -1,7 +1,7 @@
-/* $Id: LastAge.js,v 1.106 2009/05/29 03:52:33 Jim Exp $ */
+/* $Id: LastAge.js,v 1.107 2011/03/24 23:04:21 jhayes Exp $ */
 
 /*
-Copyright 2008, James J. Hayes
+Copyright 2011, James J. Hayes
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
--- LastAgePrestige.js
+++ LastAgePrestige.js
@@ -1,7 +1,7 @@
-/* $Id: LastAgePrestige.js,v 1.46 2008/04/04 05:33:20 Jim Exp $ */
+/* $Id: LastAgePrestige.js,v 1.47 2011/03/24 23:04:21 jhayes Exp $ */
 
 /*
-Copyright 2008, James J. Hayes
+Copyright 2011, James J. Hayes
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
