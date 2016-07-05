5999d31dde07308f79cdd9dfb0fa28d8d0fbd218
jodaf <spinaltap12345@gmail.com>
2016-07-04 21:52:12 -0700

    Update version.

--- LastAge.js
+++ LastAge.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 "use strict";
 
-var LASTAGE_VERSION = '1.0';
+var LASTAGE_VERSION = '1.1.0';
 
 /*
  * This module loads the rules from the Second Edition core rule book.
